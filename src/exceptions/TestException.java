577921b2db8c52eabc7e3435498b332253c66886
dzimiks <vana997@gmail.com>
2019-03-17 10:59:46 +0100

    Add new folders

---
+++ src/exceptions/TestException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class TestException {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ src/tests/TestFile.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class TestFile {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
