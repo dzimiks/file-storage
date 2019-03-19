1c2e351918ecd398620b33e2056d95964f2fb896
dzimiks <vana997@gmail.com>
2019-03-19 19:57:53 +0100

    Add GSON

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,17 @@
 package main;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonObject object = new JsonObject();
+        object.addProperty("test", 1);
+        object.addProperty("test2", "second");
+        String string = gson.toJson(object);
+        System.out.println(string);
     }
 }
