b45b6ba8fce59c371a7eaed0d83e82ef830f4109
dzimiks <vana997@gmail.com>
2019-04-09 22:36:09 +0200

    Add zip functionality

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,15 +3,24 @@ package main;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import models.Arhive;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         JsonObject object = new JsonObject();
         object.addProperty("test", 1);
         object.addProperty("test2", "second");
         String string = gson.toJson(object);
         System.out.println(string);
+
+        File file = new File("/Users/dzimiks/Desktop/projects/file-storage/src/file");
+        String path = "/Users/dzimiks/Desktop/projects/file-storage/src";
+        Arhive arhive = new Arhive();
+        arhive.zipFolder(file, path);
     }
 }
---
+++ src/main/java/models/Arhive.java
@@ -0,0 +1,103 @@
+package models;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * @author dzimiks
+ * Date: 09-04-2019 at 22:06
+ */
+public class Arhive {
+
+    /**
+     * Creates zipped file on given path.
+     *
+     * @param file File we want to zip.
+     * @param path Path where we want to save zipped file.
+     * @throws IOException Throws IOException if file doesn't exists on given path.
+     */
+    public void zipFile(File file, String path) throws IOException {
+        if (!file.isDirectory()) {
+            FileOutputStream fileOutputStream = new FileOutputStream(path + "/zipped-file.zip");
+            ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            ZipEntry zipEntry = new ZipEntry(file.getName());
+            zipOutputStream.putNextEntry(zipEntry);
+
+            byte[] bytes = new byte[1024];
+            int length;
+
+            while ((length = fileInputStream.read(bytes)) > 0) {
+                zipOutputStream.write(bytes, 0, length);
+            }
+
+            zipOutputStream.close();
+            fileInputStream.close();
+            fileOutputStream.close();
+        }
+    }
+
+    /**
+     * Creates zipped folder on given path.
+     *
+     * @param file Folder we want to zip.
+     * @param path Path where we want to save zipped folder.
+     * @throws IOException Throws IOException if folder doesn't exists on given path.
+     */
+    public void zipFolder(File file, String path) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(path + "/zipped-folder.zip");
+        ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+
+        zip(file, file.getName(), zipOutputStream);
+        zipOutputStream.close();
+        fileOutputStream.close();
+    }
+
+    /**
+     * Recursively zip a given folder.
+     *
+     * @param file            File/folder we want to zip.
+     * @param fileName        Name of given file/folder.
+     * @param zipOutputStream ZIP output stream.
+     * @throws IOException Throws IOException if ZIP output stream doesn't work correctly.
+     */
+    private void zip(File file, String fileName, ZipOutputStream zipOutputStream) throws IOException {
+        if (file.isHidden()) {
+            return;
+        }
+
+        if (file.isDirectory()) {
+            if (fileName.endsWith("/")) {
+                zipOutputStream.putNextEntry(new ZipEntry(fileName));
+                zipOutputStream.closeEntry();
+            } else {
+                zipOutputStream.putNextEntry(new ZipEntry(fileName + "/"));
+                zipOutputStream.closeEntry();
+            }
+
+            File[] children = file.listFiles();
+
+            if (children != null) {
+                for (File childFile : children) {
+                    zip(childFile, fileName + "/" + childFile.getName(), zipOutputStream);
+                }
+            }
+
+            return;
+        }
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ZipEntry zipEntry = new ZipEntry(fileName);
+        zipOutputStream.putNextEntry(zipEntry);
+        byte[] bytes = new byte[1024];
+        int length;
+
+        while ((length = fileInputStream.read(bytes)) >= 0) {
+            zipOutputStream.write(bytes, 0, length);
+        }
+
+        fileInputStream.close();
+    }
+}
+
